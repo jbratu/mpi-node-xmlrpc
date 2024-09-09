43a91cee8d19787665d91e8821551887bba974a5
EC2AMAZ-RPVSAI7\wasim <wasim@srpcs.com>
2024-09-09 13:14:03 +0000

    listLocations function changes

---
+++ example/mpi-tests-list-locations.js
@@ -0,0 +1,11 @@
+let clientOptions = "https://direct-ppro.modernparking.net/OI/oecgi3.exe/call_parkpro";
+let client = xmlrpc.createSecureClient(clientOptions)
+client.options.encoding = 'utf-8';
+
+let rpcHeaders = {
+  version: 1,
+  key: 'luome6hkd17fd2qkuj49om6sn7'
+};
+
+let locations = await client.listLocations(rpcHeaders, 'listLocations');
+console.log(locations);
